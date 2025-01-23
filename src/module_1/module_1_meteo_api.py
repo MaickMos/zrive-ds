c3c3bce9df3bae3cbbb39a72b208cf61f51d67fc
MAICK_CETIC <maick298@gmail.com>
2025-01-23 09:56:06 +0100

    Apply black and flake8

--- src/module_1/module_1_meteo_api.py
+++ src/module_1/module_1_meteo_api.py
@@ -2,8 +2,9 @@ import openmeteo_requests
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
-#the Library were taken from  https://open-meteo.com/en/docs
-#pip install openmeteo-requests #poetry add openmeteo-requests
+
+# the Library were taken from  https://open-meteo.com/en/docs
+# pip install openmeteo-requests #poetry add openmeteo-requests
 
 """First, I wrote the code using the openmeteo_requests library, which is available at https://open-meteo.com/en/docs.
 This library get the data in a little different compared to requests.
@@ -11,8 +12,8 @@ Later, I realized that it had to be done using only the requests library and a g
 So, I did another functions. I left both methods to get and process the data: requests and openmeteo_requests.
 """
 
-def graphic(weather,variables_to_graph,frequency):
 
+def graphic(weather, variables_to_graph, frequency):
     """
     Graph a dataframe in temporal series
 
@@ -21,49 +22,67 @@ def graphic(weather,variables_to_graph,frequency):
     - weather: dataframe in temporal series
     - variables_to_graph: List of the header of the column in the dataframe to graph
       ["temperature_2m_min","temperature_2m_max", "precipitation_sum", "wind_speed_10m_max"]
-    - frequency: caracter to indicates the frecuency: "Y", "M", "D" 
+    - frequency: caracter to indicates the frecuency: "Y", "M", "D"
 
     Return:
-    -A graph with subplot for each variable, let various cities 
+    -A graph with subplot for each variable, let various cities
     """
 
-    subtitle = {"temperature_2m_mean": "Average Temperature",
-                "precipitation_sum":"Sum of precipitation",
-                "wind_speed_10m_max":"Wind speed"}
-    
-    #convert the column data in data format
-    weather["date"] = pd.to_datetime(weather["date"])  
-    
-    #do the cycle for each city in the dataframe 
+    subtitle = {
+        "temperature_2m_mean": "Average Temperature",
+        "precipitation_sum": "Sum of precipitation",
+        "wind_speed_10m_max": "Wind speed",
+    }
+
+    # convert the column data in data format
+    weather["date"] = pd.to_datetime(weather["date"])
+
+    # do the cycle for each city in the dataframe
     for city, data_by_city in weather.groupby("city"):
 
-        #create a subplot for the number varaible in the list
-        fig, ax = plt.subplots(1,len(variables_to_graph),)
+        # create a subplot for the number varaible in the list
+        fig, ax = plt.subplots(
+            1,
+            len(variables_to_graph),
+        )
 
-        #obtain the mean  agroup by the frecuency
-        average_data = data_by_city.groupby(data_by_city["date"].dt.to_period(frequency))[variables_to_graph].mean()
+        # obtain the mean  agroup by the frecuency
+        average_data = data_by_city.groupby(
+            data_by_city["date"].dt.to_period(frequency)
+        )[variables_to_graph].mean()
         average_data.reset_index(inplace=True)
         average_data["date"] = average_data["date"].dt.to_timestamp()
 
-        #Do the cycle for each varaible
+        # Do the cycle for each varaible
         for i in range(len(variables_to_graph)):
-            #if there is just a plot
+            # if there is just a plot
             if len(variables_to_graph) == 1:
                 ax.set_ylabel(subtitle[variables_to_graph[i]])
-                ax.plot(average_data["date"], average_data[variables_to_graph[i]], marker = "o", label = subtitle[variables_to_graph[i]])
+                ax.plot(
+                    average_data["date"],
+                    average_data[variables_to_graph[i]],
+                    marker="o",
+                    label=subtitle[variables_to_graph[i]],
+                )
                 ax.legend()
                 ax.grid(True)
             else:
-                #if there are more than one plot
+                # if there are more than one plot
                 ax[i].set_ylabel(subtitle[variables_to_graph[i]])
-                ax[i].plot(average_data["date"], average_data[variables_to_graph[i]], marker = "o", label = subtitle[variables_to_graph[i]])
+                ax[i].plot(
+                    average_data["date"],
+                    average_data[variables_to_graph[i]],
+                    marker="o",
+                    label=subtitle[variables_to_graph[i]],
+                )
                 ax[i].legend()
                 ax[i].grid(True)
-        print(city)  
+        print(city)
         print(average_data)
         fig.suptitle(city)
-            
+
     plt.show()
+
 
 def call_api_requests(url, params):
     """
@@ -71,7 +90,7 @@ def call_api_requests(url, params):
 
     Parámetros:
     - url: the string url
-    - params: parameters for the API openmeteorequest 
+    - params: parameters for the API openmeteorequest
         Example:
             params = {
                 "latitude" :C 40.416775,
@@ -84,9 +103,9 @@ def call_api_requests(url, params):
     a type object response
     """
     try:
-        #Do the request to the client
-        response = requests.get(url,params=params)
-        #the raise for any exception of the answer
+        # Do the request to the client
+        response = requests.get(url, params=params)
+        # the raise for any exception of the answer
         response.raise_for_status()
         print(f"Status code: {response.status_code}")
 
@@ -108,14 +127,14 @@ def call_api_requests(url, params):
         print(f"Problem unexpected :{e}")
         raise
 
-def call_api_openmeteo_requests(url, params):
 
+def call_api_openmeteo_requests(url, params):
     """
     Request to the API with library openmeteo_requests
 
     Parámetros:
     - url: the string url
-    - params: parameters for the API openmeteorequest 
+    - params: parameters for the API openmeteorequest
         Example:
             params = {
                 "latitude" :C 40.416775,
@@ -129,11 +148,11 @@ def call_api_openmeteo_requests(url, params):
     """
 
     try:
-        #Do the request to the client with the library
+        # Do the request to the client with the library
         request_API_meteo = openmeteo_requests.Client()
         response = request_API_meteo.weather_api(url, params=params)
-        #this request get a list of object WeatherApiResponse
-        #request = response[0]
+        # this request get a list of object WeatherApiResponse
+        # request = response[0]
         """ The mainly problem was this response doesn't have a statuscode or raise_for_status
         so i didn't the exception for this function"""
         daily = response[0].Daily()
@@ -142,7 +161,8 @@ def call_api_openmeteo_requests(url, params):
     except Exception as e:
         print("Error in :", e)
 
-def get_data_meteo_api(cities,start_date,end_date):
+
+def get_data_meteo_api(cities, start_date, end_date):
     """
     Get the meteorology data from API openmeteo_requests
 
@@ -152,52 +172,61 @@ def get_data_meteo_api(cities,start_date,end_date):
         "2010-01-01"
     - end_date: date final to get the data
         "2020-12-31"
-    
+
     Retorna:
     A dataframe of the cities and variables
     """
 
-    #From initial code:
+    # From initial code:
     API_URL = "https://archive-api.open-meteo.com/v1/archive?"
     COORDINATES = {
-    "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
-    "London": {"latitude": 51.507351, "longitude": -0.127758},
-    "Rio": {"latitude": -22.906847, "longitude": -43.172896},}
-    VARIABLES = ["temperature_2m_min","temperature_2m_max", "precipitation_sum", "wind_speed_10m_max"]
+        "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
+        "London": {"latitude": 51.507351, "longitude": -0.127758},
+        "Rio": {"latitude": -22.906847, "longitude": -43.172896},
+    }
+    VARIABLES = [
+        "temperature_2m_min",
+        "temperature_2m_max",
+        "precipitation_sum",
+        "wind_speed_10m_max",
+    ]
 
     cities_list_dataframe = []
-        #Do the cicle for each city in list cities
+    # Do the cicle for each city in list cities
     for city in cities:
-        #Parameters for the request to the API
+        # Parameters for the request to the API
         params = {
-            "latitude" :COORDINATES[city]["latitude"],
-            "longitude":COORDINATES[city]["longitude"],
+            "latitude": COORDINATES[city]["latitude"],
+            "longitude": COORDINATES[city]["longitude"],
             "start_date": start_date,
             "end_date": end_date,
-            "daily": ",".join(VARIABLES)}
+            "daily": ",".join(VARIABLES),
+        }
 
-        response = call_api_requests(API_URL,params)
+        response = call_api_requests(API_URL, params)
 
         dicctionary_response = response.json()
         dicctionary_daily = dicctionary_response["daily"]
-        
-        df_cuidad = pd.DataFrame(dicctionary_daily)
-        df_cuidad['date'] = df_cuidad.pop('time')
-        #create a column with the city
-        df_cuidad["city"]=city
 
-        
-        #Create a dataframe and save in a list of dataframe then concat
+        df_cuidad = pd.DataFrame(dicctionary_daily)
+        df_cuidad["date"] = df_cuidad.pop("time")
+        # create a column with the city
+        df_cuidad["city"] = city
+
+        # Create a dataframe and save in a list of dataframe then concat
         df_cuidad
         cities_list_dataframe.append(df_cuidad)
-    #join the dataframe in the same dataframe
-    weather  = pd.concat(cities_list_dataframe)
-    #check  if exists the two varaibles of temperature to calculate the mean
+    # join the dataframe in the same dataframe
+    weather = pd.concat(cities_list_dataframe)
+    # check  if exists the two varaibles of temperature to calculate the mean
     if "temperature_2m_min" and "temperature_2m_max" in VARIABLES:
-        weather["temperature_2m_mean"] = (weather["temperature_2m_min"]+weather["temperature_2m_max"])/2
+        weather["temperature_2m_mean"] = (
+            weather["temperature_2m_min"] + weather["temperature_2m_max"]
+        ) / 2
     return weather
-    
-def get_data_meteo_api_openmeteo_requests(cities,start_date,end_date):
+
+
+def get_data_meteo_api_openmeteo_requests(cities, start_date, end_date):
     """
     Get the meteorology data from API openmeteo_requests
 
@@ -207,67 +236,86 @@ def get_data_meteo_api_openmeteo_requests(cities,start_date,end_date):
         "2010-01-01"
     - end_date: date final to get the data
         "2020-12-31"
-    
+
     Retorna:
     A dataframe of the cities and variables
     """
 
-    #From initial code:
+    # From initial code:
     API_URL = "https://archive-api.open-meteo.com/v1/archive?"
     COORDINATES = {
-    "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
-    "London": {"latitude": 51.507351, "longitude": -0.127758},
-    "Rio": {"latitude": -22.906847, "longitude": -43.172896},}
-    VARIABLES = ["temperature_2m_min","temperature_2m_max", "precipitation_sum", "wind_speed_10m_max"]
-    
+        "Madrid": {"latitude": 40.416775, "longitude": -3.703790},
+        "London": {"latitude": 51.507351, "longitude": -0.127758},
+        "Rio": {"latitude": -22.906847, "longitude": -43.172896},
+    }
+    VARIABLES = [
+        "temperature_2m_min",
+        "temperature_2m_max",
+        "precipitation_sum",
+        "wind_speed_10m_max",
+    ]
+
     cities_list_dataframe = []
-        #Do the cicle for each city in list cities
+    # Do the cicle for each city in list cities
     for city in cities:
-        #Parameters for the request to the API
+        # Parameters for the request to the API
         params = {
-            "latitude" :COORDINATES[city]["latitude"],
-            "longitude":COORDINATES[city]["longitude"],
+            "latitude": COORDINATES[city]["latitude"],
+            "longitude": COORDINATES[city]["longitude"],
             "start_date": start_date,
             "end_date": end_date,
-            "daily": VARIABLES}
+            "daily": VARIABLES,
+        }
 
-        #Call the api with the library
-        daily = call_api_openmeteo_requests(API_URL,params)
+        # Call the api with the library
+        daily = call_api_openmeteo_requests(API_URL, params)
 
-        #Create the column of the date since start date to final date with the frecuency dairy
-        daily_data = {"date" : pd.date_range(
-        start = pd.to_datetime(daily.Time(), unit="s", utc=True),
-        end = pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
-        freq= pd.Timedelta(seconds = daily.Interval()),inclusive="left").date}
+        # Create the column of the date since start date to final date with the frecuency dairy
+        daily_data = {
+            "date": pd.date_range(
+                start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+                end=pd.to_datetime(daily.TimeEnd(), unit="s", utc=True),
+                freq=pd.Timedelta(seconds=daily.Interval()),
+                inclusive="left",
+            ).date
+        }
 
-        #create a column with the city
+        # create a column with the city
         daily_data["city"] = city
-        #set the variables in the respective column
+        # set the variables in the respective column
         for i, variable in enumerate(VARIABLES):
             daily_data[variable] = daily.Variables(i).ValuesAsNumpy()
-        
-        #Create a dataframe and save in a list of dataframe then concat
-        daily_dataframe = pd.DataFrame(data = daily_data)
+
+        # Create a dataframe and save in a list of dataframe then concat
+        daily_dataframe = pd.DataFrame(data=daily_data)
         cities_list_dataframe.append(daily_dataframe)
-        
-    #join the dataframe in the same dataframe
-    weather  = pd.concat(cities_list_dataframe)
-    #check  if exists the two varaibles of temperature to calculate the mean
+
+    # join the dataframe in the same dataframe
+    weather = pd.concat(cities_list_dataframe)
+    # check  if exists the two varaibles of temperature to calculate the mean
     if "temperature_2m_min" and "temperature_2m_max" in VARIABLES:
-        weather["temperature_2m_mean"] = (weather["temperature_2m_min"]+weather["temperature_2m_max"])/2
-    
-    return weather    
+        weather["temperature_2m_mean"] = (
+            weather["temperature_2m_min"] + weather["temperature_2m_max"]
+        ) / 2
+
+    return weather
+
 
 def main():
-    cities = ["Madrid","London","Rio"]
+    cities = ["Madrid", "London", "Rio"]
     start_date = "2010-01-01"
     end_date = "2020-12-31"
-    variables_to_graph  = ["temperature_2m_mean", "precipitation_sum", "wind_speed_10m_max"]
+    variables_to_graph = [
+        "temperature_2m_mean",
+        "precipitation_sum",
+        "wind_speed_10m_max",
+    ]
     frequency = "A"
 
-    data = get_data_meteo_api(cities,start_date,end_date)
-    #data = get_data_meteo_api_openmeteo_requests(cities,start_date,end_date)
-    graphic(data,variables_to_graph,frequency)
+    data = get_data_meteo_api(cities, start_date, end_date)
+    # data = get_data_meteo_api_openmeteo_requests(cities,start_date,end_date)
+    graphic(data, variables_to_graph, frequency)
+
 
 if __name__ == "__main__":
     main()
--- tests/module_1/test_meteo_api.py
+++ tests/module_1/test_meteo_api.py
@@ -2,42 +2,52 @@ import unittest
 from unittest.mock import patch, Mock
 import requests
 import pandas as pd
-from src.module_1.module_1_meteo_api import graphic, call_api_requests, call_api_openmeteo_requests, get_data_meteo_api
+from src.module_1.module_1_meteo_api import (
+    graphic,
+    call_api_requests,
+    call_api_openmeteo_requests,
+    get_data_meteo_api,
+)
 
-#This unittest was powered by chatgpt, because I have never done an unittest before
-#however I tried to understand what is doing and searched every that I didn't understand
+# This unittest was powered by chatgpt, because I have never done an unittest before
+# however I tried to understand what is doing and searched every that I didn't understand
 
-#Declarate a class because unittest use class to organice the test
+
+# Declarate a class because unittest use class to organice the test
 class test_module_1_meteo_functions(unittest.TestCase):
-    #patch for "reemplace" the orginal function for the test
+    # patch for "reemplace" the orginal function for the test
     @patch("src.module_1.module_1_meteo_api.requests.get")
-    def test_call_api_requests_HTTPError(self,mock_get):
-        #Create the object mock, it's going to do the simulation of external factors
+    def test_call_api_requests_HTTPError(self, mock_get):
+        # Create the object mock, it's going to do the simulation of external factors
         mock_response = Mock()
-        #indicate whats will be the answer
-        mock_response.raise_for_status = Mock(side_effect=requests.exceptions.HTTPError("404"))
+        # indicate whats will be the answer
+        mock_response.raise_for_status = Mock(
+            side_effect=requests.exceptions.HTTPError("404")
+        )
 
-        #define what the mock have to get for a correct test
+        # define what the mock have to get for a correct test
         mock_get.return_value = mock_response
         with self.assertRaises(requests.exceptions.HTTPError):
-            call_api_requests("url",{"noting":"none"})
-        
-    #Self To reference the same object
+            call_api_requests("url", {"noting": "none"})
+
+    # Self To reference the same object
     def test_graphic(self):
         # define the data for test
-        weather = pd.DataFrame({
-            "date": pd.date_range("2020-01-01", "2020-12-31", freq="ME"),
-            "city": ["Madrid"] * 12,
-            "temperature_2m_mean": range(12),
-            "precipitation_sum": range(10, 22),
-            "wind_speed_10m_max": range(5, 17)
-        })
+        weather = pd.DataFrame(
+            {
+                "date": pd.date_range("2020-01-01", "2020-12-31", freq="ME"),
+                "city": ["Madrid"] * 12,
+                "temperature_2m_mean": range(12),
+                "precipitation_sum": range(10, 22),
+                "wind_speed_10m_max": range(5, 17),
+            }
+        )
         variables_to_graph = ["temperature_2m_mean", "precipitation_sum"]
         frequency = "M"
 
         # Test that the function runs without errors
         try:
-            #Call the function
+            # Call the function
             graphic(weather, variables_to_graph, frequency)
         except Exception as e:
             self.fail(f"graphic() raised an exception: {e}")
@@ -53,7 +63,7 @@ class test_module_1_meteo_functions(unittest.TestCase):
         # Test
         url = "https://example.com/api"
         params = {"param1": "value1"}
-        #For a correct answer 
+        # For a correct answer
         result = call_api_requests(url, params)
         self.assertEqual(result.status_code, 200)
 
